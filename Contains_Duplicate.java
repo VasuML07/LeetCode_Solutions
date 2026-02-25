a393cc5448a6a0a470c055bd93f6495d1dea2ac3
Vasu margana <aveenashvasu2024@gmail.com>
2026-02-25 18:33:14 +0530

    Create Contains_Duplicate.java

---
+++ Contains_Duplicate.java
@@ -0,0 +1,11 @@
+/*Create a hashset and now iterate through the array if element is already found in hashset
+return false if not add the element to hashset*/
+
+Set<Integer> seen = new HashSet<>();
+for(int num:nums){
+  if(seen.contains(num)){
+    return false;
+  }
+  seen.add(nums);
+}
+return false;
