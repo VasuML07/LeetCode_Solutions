5ac8c478505d267a16c09c54ac4ddc80e11c14fb
Vasu margana <aveenashvasu2024@gmail.com>
2026-02-25 21:14:29 +0530

    Add parentheses validation function
    Implement a function to check if parentheses are valid using a stack.

--- Valid_Parentheses.java
+++ Valid_Parentheses.java
@@ -1,1 +1,23 @@
+/*we will determibe if a string of brackets is valid parentheses .
+every poening bracket should be closed.brackets should be in correct order.Lets use stack because 
+for correct order of opening*/
 
+if(s.length()%2 !=0){
+  return false;
+}
+Stack<character> stack =new Stack<>();
+Map<Character,Character> pairs = new HashMap<>();
+pairs.put(')','(');
+pairs.put('}','{');
+pairs.put(']','['');
+for(char c:s.toCharArray()){
+  if(pairs.containsKey(c)){
+    if(stack.isEmpty() || stack.pop() != pairs.get(c)){
+      return false;
+    }
+  }else{
+    stack.push(c);
+  }
+}
+return stack.isEmpty();
+      
