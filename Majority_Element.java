238277d58909fd1bce0e64d32d1e83535963adb4
Vasu margana <aveenashvasu2024@gmail.com>
2026-02-25 20:20:24 +0530

    Update Majority_Element.java

--- Majority_Element.java
+++ Majority_Element.java
@@ -1,1 +1,16 @@
+/*Here for this problem majority element is defines as the element which occurs more than [n/2]
+times
+Let element be candidate maintain candidate and count.iterate through array  if count=0 set
+current number as candidate.if current number matches candidate increment coununt if not decrement it
+upto 0.final
+candidate is majority element*/
 
+int candidate = 0;
+int count = 0;
+for(int num:nums){
+  if(count==0){
+    candidate =num;
+  }
+  count+=(num==candidate) ? 1:-1;
+}
+return candidate;
