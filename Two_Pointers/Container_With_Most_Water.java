daf76201ef38fd564dc4d927d51e78677a63d872
Vasu margana <aveenashvasu2024@gmail.com>
2026-03-09 21:14:14 +0530

    Update Container_With_Most_Water.java

--- Two_Pointers/Container_With_Most_Water.java
+++ Two_Pointers/Container_With_Most_Water.java
@@ -1,1 +1,20 @@
+/*we need to find area of water contained b/w 2 lines defined by 
+Area = min(height[left],height[right])*(right-left)
+tom maximize it start left with 0 and right with n-1,calculate current area and update the 
+maximum area.move pointer  according to shorter line if we move taller line area decreases..
+*/
 
+int left = 0;
+int right = height.length-1;
+int maxwater = 0;
+while(left<right){
+  int h = Math.min(height[left],height[right]);
+  int w = right-left;
+  maxwater = Math.max(maxwater,w*h);
+  if(height[left] < height[right]){
+    left++;
+  }else{
+    right--;
+  }
+}
+return maxwater;
